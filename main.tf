dddbc02aea158f9c925c1340bb025abdc6f6e81b
Jose Virriel <jvirriel@nisum.com>
2020-02-05 16:05:29 -0300

    changes tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  project = "prueba-gke"
+  project = "gke-tf-gitlab"
   region = "us-central-1"
   zone = "us-central1-a"
 }
 
 resource "google_container_cluster" "primary" {
     name = "k8s-test"
-    location = "us-central1-a"
+    location = "us-central1"
 
     remove_default_node_pool = true
     initial_node_count       = 1
@@ -23,7 +23,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
   location   = "us-central1"
   cluster    = google_container_cluster.primary.name
-  node_count = 3
+  node_count = 1
   node_config {
     preemptible  = true
     machine_type = "n1-standard-1"
