2372deefd0b74ea1ed3b08ab9a934e966b7ceec0
Jose Virriel <jvirriel@nisum.com>
2020-02-05 14:36:31 -0300

    terraform

--- main.tf
+++ main.tf
@@ -8,6 +8,9 @@ resource "google_container_cluster" "primary" {
     name = "k8s-test"
     location = "us-central1-a"
 
+    remove_default_node_pool = true
+    initial_node_count       = 1
+
     master_auth {
     username = ""
     password = ""
